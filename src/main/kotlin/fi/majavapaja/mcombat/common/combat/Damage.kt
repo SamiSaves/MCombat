479082cb8c2983b134064218a491e69d6a9e0338
= <sami_eklund94@hotmail.com>
2019-07-07 21:04:25 +0300

    Zombies now have damageType capability set on spawn

--- src/main/kotlin/fi/majavapaja/mcombat/common/combat/Damage.kt
+++ src/main/kotlin/fi/majavapaja/mcombat/common/combat/Damage.kt
@@ -3,11 +3,15 @@ package fi.majavapaja.mcombat.common.combat
 import fi.majavapaja.mcombat.modId
 import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityLiving
+import net.minecraft.entity.monster.EntityMob
+import net.minecraft.entity.monster.EntityZombie
 import net.minecraft.util.ResourceLocation
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.common.capabilities.CapabilityManager
 import net.minecraftforge.event.AttachCapabilitiesEvent
+import net.minecraftforge.event.entity.EntityJoinWorldEvent
 import net.minecraftforge.event.entity.living.LivingHurtEvent
+import net.minecraftforge.event.entity.living.LivingSpawnEvent
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.eventhandler.EventPriority
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
@@ -30,6 +34,16 @@ object Damage {
   fun attachCapability(event: AttachCapabilitiesEvent<Entity>) {
     if (event.`object` is EntityLiving) {
       event.addCapability(damageTypeResource, DamageTypeProvider())
+    }
+  }
+
+  @SubscribeEvent
+  fun onEntitySpawn(event: EntityJoinWorldEvent) {
+    val entity = event.entity
+    if (entity is EntityMob && entity is EntityZombie) {
+      val damageType = DamageTypeProvider.getDamageType(entity)
+      damageType?.type = "rotten"
+      println("Damage type set ${damageType?.type}")
     }
   }
 }
