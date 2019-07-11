c9d83269f395d12de1c44601c2da50d1905cb971
= <sami_eklund94@hotmail.com>
2019-07-11 22:48:41 +0300

    Added debug armor

--- src/main/kotlin/fi/majavapaja/mcombat/common/item/ModItems.kt
+++ src/main/kotlin/fi/majavapaja/mcombat/common/item/ModItems.kt
@@ -1,18 +1,34 @@
 package fi.majavapaja.mcombat.common.item
 
+import net.minecraft.inventory.EntityEquipmentSlot
 import net.minecraft.item.Item
 import net.minecraftforge.registries.IForgeRegistry
 
 object ModItems {
   val majavapaja: ItemBase = ItemBase("majavapaja")
   val debugSword: ItemSword = ItemSword("debug_sword", Item.ToolMaterial.WOOD)
+  val debugChestplate = ArmorBase("debug_chestplate", ArmorBase.debugMaterial, EntityEquipmentSlot.CHEST)
+  val debugLeggings = ArmorBase("debug_leggings", ArmorBase.debugMaterial, EntityEquipmentSlot.LEGS)
+  val debugBoots = ArmorBase("debug_boots", ArmorBase.debugMaterial, EntityEquipmentSlot.FEET)
+  val debugHelmet = ArmorBase("debug_helmet", ArmorBase.debugMaterial, EntityEquipmentSlot.HEAD)
 
   fun register(registry: IForgeRegistry<Item>) {
-    registry.registerAll(majavapaja, debugSword)
+    registry.registerAll(
+        majavapaja,
+        debugSword,
+        debugChestplate,
+        debugLeggings,
+        debugBoots,
+        debugHelmet
+    )
   }
 
   fun registerModels() {
     majavapaja.registerItemModel()
     debugSword.registerItemModel()
+    debugChestplate.registerItemModel()
+    debugLeggings.registerItemModel()
+    debugBoots.registerItemModel()
+    debugHelmet.registerItemModel()
   }
 }
