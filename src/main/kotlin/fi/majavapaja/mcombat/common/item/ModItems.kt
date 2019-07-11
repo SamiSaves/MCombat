cf23e7d8c0ad0983d0319dcc4ab557dfc6b96079
= <sami_eklund94@hotmail.com>
2019-07-11 08:09:38 +0300

    Created new sword, debug_sword

--- src/main/kotlin/fi/majavapaja/mcombat/common/item/ModItems.kt
+++ src/main/kotlin/fi/majavapaja/mcombat/common/item/ModItems.kt
@@ -5,12 +5,14 @@ import net.minecraftforge.registries.IForgeRegistry
 
 object ModItems {
   val majavapaja: ItemBase = ItemBase("majavapaja")
+  private val debugSword: ItemSword = ItemSword("debug_sword", Item.ToolMaterial.WOOD)
 
   fun register(registry: IForgeRegistry<Item>) {
-    registry.registerAll(majavapaja)
+    registry.registerAll(majavapaja, debugSword)
   }
 
   fun registerModels() {
     majavapaja.registerItemModel()
+    debugSword.registerItemModel()
   }
 }
