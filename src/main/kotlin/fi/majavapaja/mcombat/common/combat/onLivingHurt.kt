5b52696f738e1e06cfccae7e96b62288e3f68521
= <sami_eklund94@hotmail.com>
2019-07-14 18:46:43 +0300

    Moved logic of damageType to a separate function

--- src/main/kotlin/fi/majavapaja/mcombat/common/combat/onLivingHurt.kt
+++ src/main/kotlin/fi/majavapaja/mcombat/common/combat/onLivingHurt.kt
@@ -1,27 +1,18 @@
 package fi.majavapaja.mcombat.common.combat
 
 import fi.majavapaja.mcombat.common.effect.ModEffects
+import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityLivingBase
+import net.minecraft.entity.projectile.EntityArrow
 import net.minecraftforge.event.entity.living.LivingHurtEvent
 
 fun onLivingHurtEvent(event: LivingHurtEvent) {
   event.isCanceled = true
   val entity = event.entity as EntityLivingBase
+  val damageType = getDamageType(event.source.trueSource, event.source.immediateSource)
 
   if (event.source.trueSource is EntityLivingBase) {
-    val trueSource = event.source.trueSource as EntityLivingBase
-    val weapon = trueSource.heldItemMainhand
-    var damage: Float
-    var damageType: DamageType?
-
-    if (!weapon.isEmpty) {
-      damageType = DamageTypeProvider.getDamageType(weapon)
-      damage = 10f
-    } else {
-      damageType = DamageTypeProvider.getDamageType(trueSource)
-      damage = 6f
-    }
-
+    var damage = 10f
     var resistanceAmount = 0f
 
     entity.armorInventoryList
@@ -54,4 +45,22 @@ fun onLivingHurtEvent(event: LivingHurtEvent) {
   }
 
   entity.health -= 2f
+}
+
+private fun getDamageType(trueSource: Entity?, immediateSource: Entity?): DamageType {
+  var damageType: DamageType? = null
+
+  if (immediateSource is EntityArrow) {
+    damageType = DamageTypeProvider.getDamageType(immediateSource)
+  } else if (trueSource is EntityLivingBase) {
+    val weapon = trueSource.heldItemMainhand
+
+    damageType = if (!weapon.isEmpty) {
+      DamageTypeProvider.getDamageType(weapon)
+    } else {
+      DamageTypeProvider.getDamageType(trueSource)
+    }
+  }
+
+  return damageType ?: DamageType()
 }
