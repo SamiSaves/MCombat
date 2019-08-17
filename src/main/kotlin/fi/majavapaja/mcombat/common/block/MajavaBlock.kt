43e6a1c7cda650674c838f8c8703415a6156c717
= <sami_eklund94@hotmail.com>
2019-08-17 19:36:30 +0300

    Cleaned up MajavaBlock

--- src/main/kotlin/fi/majavapaja/mcombat/common/block/MajavaBlock.kt
+++ src/main/kotlin/fi/majavapaja/mcombat/common/block/MajavaBlock.kt
@@ -1,21 +1,12 @@
 package fi.majavapaja.mcombat.common.block
 
 import fi.majavapaja.mcombat.Main
-import net.minecraft.block.Block
 import net.minecraft.block.material.Material
-import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.util.BlockRenderLayer
-import net.minecraftforge.fml.relauncher.Side
-import net.minecraftforge.fml.relauncher.SideOnly
-
 
 class MajavaBlock : BaseBlock(
   name = "majava_block",
   creativeTab = Main.creativeTab,
   material = Material.ROCK,
   layer = BlockRenderLayer.TRANSLUCENT
-) {
-  init {
-    println("Initializing MajavaBlock")
-  }
-}
+)
