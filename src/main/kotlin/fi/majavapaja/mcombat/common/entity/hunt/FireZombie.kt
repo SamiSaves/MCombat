9669c6eb749d88b591fedaca093e93fbed88fed6
= <sami_eklund94@hotmail.com>
2019-09-18 07:27:51 +0300

    Add hunt progress on FireZombie death

--- src/main/kotlin/fi/majavapaja/mcombat/common/entity/hunt/FireZombie.kt
+++ src/main/kotlin/fi/majavapaja/mcombat/common/entity/hunt/FireZombie.kt
@@ -4,9 +4,12 @@ import fi.majavapaja.mcombat.Main
 import fi.majavapaja.mcombat.common.combat.DamageType
 import fi.majavapaja.mcombat.common.entity.ICustomMob
 import net.minecraft.entity.monster.EntityZombie
+import net.minecraft.entity.player.EntityPlayerMP
 import net.minecraft.util.DamageSource
 import net.minecraft.util.ResourceLocation
 import net.minecraft.world.World
+import net.minecraft.world.WorldServer
+import net.minecraftforge.common.DimensionManager
 import net.minecraftforge.fml.common.registry.EntityRegistry
 
 class FireZombie(world: World): EntityZombie(world), ICustomMob {
@@ -35,4 +38,21 @@ class FireZombie(world: World): EntityZombie(world), ICustomMob {
   }
 
   override fun isBurning(): Boolean = true
+
+  override fun onDeath(cause: DamageSource) {
+    super.onDeath(cause)
+
+    val player = cause.trueSource
+    if (player is EntityPlayerMP) {
+      val world = DimensionManager.getWorld(0) as WorldServer
+      val advancement = world.advancementManager.getAdvancement(ResourceLocation("mcombat:hunt/hunt"))
+      val huntProggress = player.advancements.getProgress(advancement)
+      if (huntProggress.completedCriteria.contains("start_quest")) {
+        huntProggress.grantCriterion("defeat_hunt")
+      } else {
+        println("Killing monster doesn't do anything if player has not started the hunt >:(")
+      }
+
+    }
+  }
 }
